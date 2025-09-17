92bb5c01780587a63fb0f2f1c656ab4669abc826
ritesh <riteshpatidar088@gmail.com>
2025-09-17 15:45:34 +0530

    updated till the task

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 function App() {
-const [list , setList] = useState([1,2,3,4,5])
-const [input , setInput] = useState('');
-
-const handleChange = (e) => {
-  console.log(e.target.value)
-  setInput(e.target.value)
-}
-console.log(list)
+  const [color, setColors] = useState(['Red', 'Orange']);
+  const [bgColor, setBgColor] = useState('');
+  const handleClick = (color) => {
+    setBgColor(color);
+  };
   return (
     <div>
-    {list.map((el)=>(
-      <h1>{el}</h1>
-    ))}
-    <form>
-      <input onChange={handleChange} type='text' placeholder='Add list item' name='text'/>
-      <button>Add</button>
-    </form>
+      {color.map((color) => (
+        <button
+          style={{ backgroundColor: bgColor }}
+          onClick={() => handleClick(color)}
+        >
+          {color}
+        </button>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
-
-
-//TASK CREATE A LIST AND SHOW ON THE UI TAKES INPUT FIELD AND A ADD BUTTON , GET THE INPUT VALUE AND ADD INTO THE LIST :
+export default App;
