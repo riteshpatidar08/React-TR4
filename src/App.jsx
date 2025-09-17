cdd91542f93fb9322246b0b4f2e62fafe1169a28
ritesh <riteshpatidar088@gmail.com>
2025-09-17 15:50:27 +0530

    udpated till aray updation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 function App() {
   const [color, setColors] = useState(['Red', 'Orange']);
+
   const [bgColor, setBgColor] = useState('');
   const handleClick = (color) => {
     setBgColor(color);
