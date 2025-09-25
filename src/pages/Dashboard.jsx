ca5504ae0553a5ffd075e6c645a1f1ca1b37dbff
ritesh <riteshpatidar088@gmail.com>
2025-09-25 13:46:16 +0530

    updated till the nested routing

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link  , Outlet} from 'react-router-dom';
 function Dashboard() {
   return (
-    <div>
+    <div className='flex'>
       <aside className="w-84 bg-sky-700 h-screen flex flex-col justify-center items-center text-white font-semibold text-lg gap-5">
         <Link to="/dashboard/overview">Overview</Link>
         <Link>Integrations</Link>
         <Link>Account</Link>
       </aside>
+      <div className='flex-1'>
+      <Outlet/>
+      </div>
     </div>
   );
 }
