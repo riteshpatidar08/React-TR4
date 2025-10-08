6161d429a9577935d91a1c06dbdac6581a2f5ca4
ritesh <riteshpatidar088@gmail.com>
2025-10-08 14:17:29 +0530

    updated tilll redux

---
+++ src/redux/slices/CounterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state, action) => {
+      state.count += 1;
+    },
+    decrement: (state, action) => {
+      state.count -= 1;
+    },
+  },
+});
