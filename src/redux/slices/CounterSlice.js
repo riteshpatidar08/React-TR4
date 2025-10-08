fd435ca1fdb740214866a22ccc3b5efe16bae054
ritesh <riteshpatidar088@gmail.com>
2025-10-08 15:46:23 +0530

    udated till till redux

--- src/redux/slices/CounterSlice.js
+++ src/redux/slices/CounterSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: { count: 1},
+  initialState: { count: 1 },
   reducers: {
     increment: (state, action) => {
       state.count += 1;
@@ -13,7 +13,17 @@ const counterSlice = createSlice({
   },
 });
 
-console.log(counterSlice)
+console.log(counterSlice);
 
-export default counterSlice.reducer
-export const {increment,decrement} = counterSlice.actions
+export default counterSlice.reducer;
+export const { increment, decrement } = counterSlice.actions;
+
+
+//TASK convert the useReducer api calling logic in to redux toolkit
+
+//todoslice 
+//initial state {loading : falsee , todos : null  , error : null}
+//reducers : => pending , fulfilled , rejected 
+//export default todoSlice.reducer
+//export const {pending , fulfilled , rejected} = todoSlice.actions 
+//connect the todoReducer with store
