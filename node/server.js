db96f8b539df1f9d676ee8fcac83477ee64bca69
ritesh <riteshpatidar088@gmail.com>
2025-10-10 14:56:20 +0530

    udpated till node

--- node/server.js
+++ node/server.js
@@ -1,10 +1,15 @@
-const http =require('http');
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  if (req.url === '/product') {
+    return res.end('products')
+  } else {
+    return res.end('hello from server');
+  }
+});
 
-const server = http.createServer((req,res)=>{
+server.listen(3000, () => {
+  console.log('server is running on 3000');
+});
 
-})
-
-server.listen(3000,()=>{
-    console.log('server is running on 3000')
-})
+//note npm i -g nodemon
