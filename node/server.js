17483fea8ff526949ac9207efb2c6f99ab5eb386
ritesh <riteshpatidar088@gmail.com>
2025-10-10 16:00:10 +0530

    udapted till api

--- node/server.js
+++ node/server.js
@@ -2,7 +2,19 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/product') {
-    return res.end('products')
+    // res.statusCode = 201;
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+    return res.end('products');
+  } else if (req.url === '/users') {
+    const fs = require('fs');
+    fs.readFile('./user.json', 'utf-8', (err, data) => {
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      res.end(data);
+    });
   } else {
     return res.end('hello from server');
   }
