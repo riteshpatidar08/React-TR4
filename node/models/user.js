cfa63adb5a2a805f707ec11e859f352b81fa6232
ritesh <riteshpatidar088@gmail.com>
2025-10-16 09:53:47 +0530

    added updated user api

--- node/models/user.js
+++ node/models/user.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String },
-    age: { type: Number },
-    email: { type: String },
-    isActive: { type: Boolean, default: true },
-  });
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Name is too short'],
+    maxlength: [30, 'Name is too long'],
+  },
+  email: { type: String, unqiue: true, required: true },
+  password: { type: String, required: true, minlength: [6, 'Password is too short'] },
+  isActive: { type: Boolean, default: true },
+  lastLogin: { type: Date },
+  createdAt: { type: Date, default: Date.now() },
+});
 
-  const User = mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema);
 
-  module.exports = User
+module.exports = User;
