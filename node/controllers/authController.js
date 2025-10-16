c32dcf2cf1592d7e386f6bb368b1d747481a1e21
ritesh <riteshpatidar088@gmail.com>
2025-10-16 10:33:06 +0530

    added signup api

--- node/controllers/authController.js
+++ node/controllers/authController.js
@@ -1,7 +1,24 @@
-exports.signUp = async(req,res)=> {
-    try {
-        
-    } catch (error) {
-        
-    }
+const User = require('./../models/user.js')
+
+exports.signUp = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+    //check if user exist , if user exist send a error msg you are already registered , please login
+const user = await User.findOne({email}) ;
+console.log(user)
+if(user){
+    const error = new Error('User is already registered , Please login')
+    throw error
 }
+
+const newUser = await User.create({name , email , password}) ;
+res.status(201).json({
+  message : "user registred successfull" ,
+  data  : newUser
+})
+  } catch (error) {
+    res.status(500).json({
+      message : error.message
+    })
+  }
+};
