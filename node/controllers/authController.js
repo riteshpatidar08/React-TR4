67b42d35c56748704732f806b9c2452324f68a04
ritesh <riteshpatidar088@gmail.com>
2025-10-16 10:56:30 +0530

    added signup api

--- node/controllers/authController.js
+++ node/controllers/authController.js
@@ -1,24 +1,40 @@
-const User = require('./../models/user.js')
+const User = require('./../models/user.js');
 
 exports.signUp = async (req, res) => {
   try {
     const { email, name, password } = req.body;
     //check if user exist , if user exist send a error msg you are already registered , please login
-const user = await User.findOne({email}) ;
-console.log(user)
-if(user){
-    const error = new Error('User is already registered , Please login')
-    throw error
-}
+    const user = await User.findOne({ email });
+    console.log(user);
+    if (user) {
+      const error = new Error('User is already registered , Please login');
+      throw error;
+    }
 
-const newUser = await User.create({name , email , password}) ;
-res.status(201).json({
-  message : "user registred successfull" ,
-  data  : newUser
-})
+    const newUser = await User.create({ name, email, password });
+    res.status(201).json({
+      message: 'user registred successfull',
+      data: newUser,
+    });
   } catch (error) {
     res.status(500).json({
-      message : error.message
-    })
+      message: error.message,
+    });
   }
 };
+
+
+exports.login = async(req,res) => {
+  try {
+    const {email , password}  = req.body ;
+    //if email exists , 
+    const user = await User.findOne({email}) ;
+    if(!user){
+      const error = new Error('User is not registered please register and try again')
+      throw error
+    }
+    console.log(user)
+  } catch (error) {
+    
+  }
+}
--- node/routes/authRoutes.js
+++ node/routes/authRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router() ;
 const authController = require('./../controllers/authController.js')
 
 //signup 
-router.post('/user' ,authController.signUp )
-
+router.post('/register' ,authController.signUp )
+router.post('/login' , authController.login)
 module.exports = router
