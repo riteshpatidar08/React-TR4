739432e151b1f5f16eb613cb9308d8d01a981b8e
ritesh <riteshpatidar088@gmail.com>
2025-10-16 10:46:20 +0530

    added signup api

--- node/models/user.js
+++ node/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -8,10 +8,20 @@ const userSchema = new mongoose.Schema({
     maxlength: [30, 'Name is too long'],
   },
   email: { type: String, unqiue: true, required: true },
-  password: { type: String, required: true, minlength: [6, 'Password is too short'] },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Password is too short'],
+  },
   isActive: { type: Boolean, default: true },
   lastLogin: { type: Date },
   createdAt: { type: Date, default: Date.now() },
+});
+
+userSchema.pre('save', async function (next) {
+  console.log(this);
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 const User = mongoose.model('user', userSchema);
